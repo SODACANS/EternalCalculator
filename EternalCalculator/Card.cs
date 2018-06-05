11b1a18c8b93c4df45a3a696a48f2022fc1a8655
Kyle <kyle.evitts@gmail.com>
2018-06-05 01:16:39 -0700

    Card.cs: Added shiftstone value functions.

--- EternalCalculator/Card.cs
+++ EternalCalculator/Card.cs
@@ -25,5 +25,79 @@ namespace EternalCalculator
         {
             return new Card(Name, Set, Rarity, IsPremium, Quantity);
         }
+
+        public int GetShiftStoneValue()
+        {
+            if (IsPremium)
+            {
+                switch (Rarity)
+                {
+                    case Rarity.Common:
+                        return 25;
+                    case Rarity.Uncommon:
+                        return 50;
+                    case Rarity.Rare:
+                        return 800;
+                    case Rarity.Legendary:
+                        return 3200;
+                    case Rarity.Promo:
+                        return 400;
+                }
+            }
+            else
+            {
+                switch (Rarity)
+                {
+                    case Rarity.Common:
+                        return 1;
+                    case Rarity.Uncommon:
+                        return 10;
+                    case Rarity.Rare:
+                        return 200;
+                    case Rarity.Legendary:
+                        return 800;
+                    case Rarity.Promo:
+                        return 100;
+                }
+            }
+            return 0;
+        }
+
+        public int GetShiftStoneCost()
+        {
+            if (IsPremium)
+            {
+                switch (Rarity)
+                {
+                    case Rarity.Common:
+                        return 800;
+                    case Rarity.Uncommon:
+                        return 1600;
+                    case Rarity.Rare:
+                        return 3200;
+                    case Rarity.Legendary:
+                        return 9600;
+                    case Rarity.Promo:
+                        return 2400;
+                }
+            }
+            else
+            {
+                switch (Rarity)
+                {
+                    case Rarity.Common:
+                        return 50;
+                    case Rarity.Uncommon:
+                        return 100;
+                    case Rarity.Rare:
+                        return 800;
+                    case Rarity.Legendary:
+                        return 3200;
+                    case Rarity.Promo:
+                        return 600;
+                }
+            }
+            return 0;
+        }
     }
 }
